d724e36520d68ed0378fc881c9607fd330a32828
Brian Anikayi <61684414+brianchemase@users.noreply.github.com>
2022-11-13 17:45:21 +0300

    Update on tasks

--- app/Http/Controllers/BotManController.php
+++ app/Http/Controllers/BotManController.php
@@ -19,9 +19,12 @@ class BotManController extends Controller
             if ($message == 'Hi') {
                 $this->askName($botman);
             }
+            if ($message == '1') {
+                $this->askPin($botman);
+            }
             
             else{
-                $botman->reply("Write 'Hi' for testing...");
+                $botman->reply("Invalid Entry .<br> Select from the following options,<br> 1. Get PIN Number <br> 2. Check Filling Status <br> 3. Get TSO Location");
             }
    
         });
@@ -39,6 +42,48 @@ class BotManController extends Controller
             $name = $answer->getText();
    
             $this->say('Nice to meet you '.$name);
+        });
+    }
+
+
+    public function askPin($botman)
+    {
+        $botman->ask('Hello! What is your ID Number?', function(Answer $answer) {
+   
+            $IDNo = $answer->getText();
+        $id_no=$IDNo;
+        
+
+            $ch = curl_init();
+        $url="http://briananikayi.io.ke/kraapi/users_api.php?id=$id_no";
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+
+        $resp= curl_exec($ch);
+        //echo $resp;
+        if ($e= curl_error($ch)) {
+            echo $e;
+            $this->say('There was an error code '.$e);
+        } else {
+            $decoded=json_decode($resp);
+            //print_r($decoded);
+            $responsecode_code = $decoded->response_code;
+            $pin=$decoded->pin;
+            $majina = $decoded->names;
+            $messageback = $decoded->message;
+
+
+           // return $messageback;
+            $response_desc = $decoded->response_code;
+	        extract(json_decode($resp, true));
+            $this->say('Dear '.$majina.'<br>your request has been received. Your Pin will be delivered to your email.<br> Thank you for contacting KRA Chat Bot <br> ID No: '.$IDNo. ' <br> Pin No: '.$pin.' <br> Names: '.$majina);
+        }
+   
+            
         });
     }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,17 +3,25 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Botman Chatbot in Laravel - Real Programmer</title>
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <title>KRA Chat Bot innovation</title>
+        {{-- <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet"> --}}
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     </head>
     <body>
+        <div class="container">
+            <img src="https://images.unsplash.com/photo-1577563908411-5077b6dc7624?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" class="img-fluid" alt="Responsive image">
+        </div>
     </body>
    
     <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/botman-web-widget@0/build/assets/css/chat.min.css">
     <script>
         var botmanWidget = {
-            aboutText: 'Write Something',
-            introMessage: "✋ Hi! I'm form Real Programmer"
+            aboutText: 'Reply to begin conversation',
+            title:'KRA Chat Bot',
+            mainColor:'#c02026',
+            bubbleBackground:'#c02026',
+            headerTextColor: '#fff',
+            introMessage: "✋ Hi! Karibu KRA .<br> Select from the following options,<br> 1. Get PIN Number <br> 2. Check Filling Status <br> 3. Get TSO Location"
         };
     </script>
    
